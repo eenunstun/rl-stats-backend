d7100d4d81090076f2a4f2e0bdc054ded56d73b9
eenunstun <eenustun19@ku.edu.tr>
2026-06-09 00:32:08 +0300

    Player table populated.

---
+++ database/seed.sql
@@ -0,0 +1,50 @@
+INSERT INTO APP_USER(user_id, username, password, is_admin)
+VALUES(1, 'Evrim', 'SoccerCar@2358', TRUE),
+      (2, 'Hakan', 'Psswd&&3457', FALSE),
+      (3, 'Leyla', '478018476@', FALSE),
+      (4, 'Rodrigo', 'RocketLeagueNo1Fan', FALSE),
+      (5, 'Ted', 'Password123', FALSE);
+
+INSERT INTO PLAYER(player_id, player_name, platform)
+VALUES(1, 'stizzy', 'PC'),
+      (2, 'ApparentlyJack','PC'),
+      (3, 'Joreuz', 'PC'),
+      (4, 'Fever', 'PC'),
+      (5, 'Torsos', 'PC');
+      (6, 'bananahead', 'PC'),
+      (7, 'Atomic', 'PS'),
+      (8, 'Daniel', 'PS'),
+      (9, 'BeastMode', 'PS');
+      (10, 'M0nkey M00n', 'PS'),
+      (11, 'dralii', 'PS'),
+      (12, 'ExoTiiK', 'PS'),
+      (13, 'Alpha54', 'PS');
+      (14, 'Radosin', 'PS'),
+      (15, 'zen', 'PS'),
+      (16, 'AcroniK', 'PS'),
+      (17, 'Oski', 'PS');
+      (18, 'Atow.', 'PS'),
+      (19, 'Firstkiller', 'XBOX'),
+      (20, 'Sypical', 'XBOX'),
+      (21, 'mist', 'XBOX');
+      (22, 'DRUFINHO', 'XBOX'),
+      (23, 'droppz', 'XBOX'),
+      (24, 'brad', 'XBOX'),
+
+INSERT INTO TEAM(team_id, team_name, region)
+VALUES(),
+
+INSERT INTO TOURNAMENT(tournament_id, tournament_name, country, start_date, end_date)
+VALUES(),
+
+INSERT INTO ARENA(arena_id, arena_name)
+VALUES(),
+
+INSERT INTO MATCH_DATA()
+VALUES(),
+
+INSERT INTO PLAYER_MATCH_STATS()
+VALUES(),
+
+INSERT INTO ()
+VALUES(),
