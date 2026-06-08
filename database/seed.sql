54e03904194fac627c5be5fae1deae3072782c6c
eenunstun <eenustun19@ku.edu.tr>
2026-06-09 00:48:33 +0300

    Arena table populated. Comma issues fixed.

--- database/seed.sql
+++ database/seed.sql
@@ -10,26 +10,26 @@ VALUES(1, 'stizzy', 'PC'),
       (2, 'ApparentlyJack','PC'),
       (3, 'Joreuz', 'PC'),
       (4, 'Fever', 'PC'),
-      (5, 'Torsos', 'PC');
+      (5, 'Torsos', 'PC'),
       (6, 'bananahead', 'PC'),
       (7, 'Atomic', 'PS'),
       (8, 'Daniel', 'PS'),
-      (9, 'BeastMode', 'PS');
+      (9, 'BeastMode', 'PS'),
       (10, 'M0nkey M00n', 'PS'),
       (11, 'dralii', 'PS'),
       (12, 'ExoTiiK', 'PS'),
-      (13, 'Alpha54', 'PS');
+      (13, 'Alpha54', 'PS'),
       (14, 'Radosin', 'PS'),
       (15, 'zen', 'PS'),
       (16, 'AcroniK', 'PS'),
-      (17, 'Oski', 'PS');
+      (17, 'Oski', 'PS'),
       (18, 'Atow.', 'PS'),
       (19, 'Firstkiller', 'XBOX'),
       (20, 'Sypical', 'XBOX'),
       (21, 'mist', 'XBOX');
       (22, 'DRUFINHO', 'XBOX'),
       (23, 'droppz', 'XBOX'),
-      (24, 'brad', 'XBOX'),
+      (24, 'brad', 'XBOX');
 
 INSERT INTO TEAM(team_id, team_name, region)
 VALUES(1, 'Dignitas', 'Europe'),
@@ -39,15 +39,19 @@ VALUES(1, 'Dignitas', 'Europe'),
       (5, 'KRÜ Esports', 'South America'),
       (6, 'G2 Esports', 'North America'),
       (7, 'Twisted Minds', 'Europe'),
-      (8, 'Karmine Corp', 'Europe'),    
+      (8, 'Karmine Corp', 'Europe');  
 
 INSERT INTO TOURNAMENT(tournament_id, tournament_name, country, start_date, end_date)
 VALUES(1, 'RLCS 2025 - World Championship', 'France', '2025-09-10', '2025-09-14'),
       (2, 'RLCS 2024 - World Championship', 'USA', '2024-09-10', '2024-09-15'),
-      (3, 'RLCS 2022-23 - World Championship', 'Germany', '2023-08-03', '2023-08-13'),
+      (3, 'RLCS 2022-23 - World Championship', 'Germany', '2023-08-03', '2023-08-13');
 
 INSERT INTO ARENA(arena_id, arena_name)
-VALUES(),
+VALUES(1, 'LDLC Arena'),
+      (2, 'Fort Worth'),
+      (3, 'PSD Bank Dome'),
+      (4, 'Paris La Défense Arena'),
+      (5, 'Lenovo Center');
 
 INSERT INTO MATCH_DATA()
 VALUES(),
@@ -55,5 +59,3 @@ VALUES(),
 INSERT INTO PLAYER_MATCH_STATS()
 VALUES(),
 
-INSERT INTO ()
-VALUES(),
